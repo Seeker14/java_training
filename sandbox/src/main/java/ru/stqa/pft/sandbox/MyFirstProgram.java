1d8b165cb96185f62e95a28cde23e0e89fdcbdab
Seeker14 <allegoriy@list.ru>
2020-11-03 22:53:17 +0300

    Added class Point and result of task №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,23 +9,25 @@ public class MyFirstProgram {
     hello("user");
     hello("Lena");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    //Задание №2
+    Point q1 = new Point(7, 2);
+    Point q2 = new Point(10, 6);
+    System.out.println("Расстояние между точками координат = " + q1.distance(q2));
   }
 
   public static void hello (String somebody){
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len){
-    return len * len;
-  }
+  //Задание №2 - функция, вычисляющая расстояние между двумя точками
+ /* public static double distance(Point p1, Point p2){
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  } */
 
-  public static double area(double a, double b){
-    return a * b;
-  }
 }
