31241bdf17ad5f5f217cc4ce3d43a3f0a8134c2b
Seeker14 <allegoriy@list.ru>
2020-12-17 03:38:48 +0300

    Added Hamcrest for Contacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class ContactModificationTests extends TestBase{
 
@@ -33,7 +35,7 @@ public class ContactModificationTests extends TestBase{
 
   @Test
   public void testContactModification(){
-    Set<ContactData> before = app.contact().all();
+    Contacts before = app.contact().all();
     ContactData modifiedContact = before.iterator().next();
     ContactData contact = new ContactData()
             .withId(modifiedContact.getId())
@@ -52,12 +54,9 @@ public class ContactModificationTests extends TestBase{
             .withGroup("test1")
             .withHomeaddress("Moscow, Gagarina street, house 9, apartment 180");
     app.contact().modify(contact);
-    Set<ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(), before.size());
-
-    before.remove(modifiedContact);
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.contact().all();
+    assertEquals(after.size(), before.size());
+    assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -24,7 +24,7 @@ public class GroupModificationTests extends TestBase {
     }
   }
 
-  @Test (enabled = false)
+  @Test
   public void testGroupModification(){
     Groups before = app.group().all();
     GroupData modifiedGroup = before.iterator().next();
