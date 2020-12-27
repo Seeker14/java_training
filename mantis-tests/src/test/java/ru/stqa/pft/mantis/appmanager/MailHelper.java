cda45b8963d967090161b836fea2b04981c36410
Seeker14 <allegoriy@list.ru>
2020-12-27 23:49:40 +0300

    Added Test for receiving mail to the mail server build into the tests

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -0,0 +1,63 @@
+package ru.stqa.pft.mantis.appmanager;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MailHelper {
+
+  private ApplicationManager app;
+  private final Wiser wiser;
+
+  //инициализация почтового сервера Wiser
+  public MailHelper(ApplicationManager app) {
+    this.app = app;
+    wiser = new Wiser();
+  }
+
+  //ожидание почтового сообщения (кол-во писем, котороые должно придти; время, в течение которого ждём)
+  public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
+    long start = System.currentTimeMillis();                                                          //запоминаем текущее время
+    while (System.currentTimeMillis() < start + timeout) {                                            //проверка в цикле, что время ожидания ещё не истекло
+      if (wiser.getMessages().size() >= count) {                                                      //проверка в цикле, если кол-во писем прило достаточно - ожидаение прекращается
+        return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());  //преобразование реальных объектов в модельные
+      }
+      try {
+        Thread.sleep(1000);                                                                      //подождать 1000 миллисекунд, если кол-во почты пришло не достаточно, чтобы завершить цикл выше. Снова возвращаемся к циклу выше, пока не придёт нужное кол-во писем или время ожиданя не истечёт
+      } catch (InterruptedException ex) {
+        ex.printStackTrace();
+      }
+    }
+    throw new Error("No mail :(");
+  }
+
+  //mantis присылает письма в текстовом формате
+  //при регистрации mantis присылает 2 письма: (1) администратору, (2) пользователю
+  public static MailMessage toModelMail(WiserMessage m) {
+    try {
+      MimeMessage mm = m.getMimeMessage();
+      return new MailMessage(mm.getAllRecipients()[0].toString(), (String) mm.getContent());   //взяли список получателей, оставили 1 из них, поскольку он единственный; преобразовали в строку, поскольку письмо текстовое => попадает в модельный объект
+    } catch (MessagingException ex) {                                                          // оставшаяся часть - перехват ошибок, которые могут возникнуть
+      ex.printStackTrace();
+      return null;
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      return null;
+    }
+  }
+
+  //запуск почтового сервера Wiser
+  public void start() {
+    wiser.start();
+  }
+
+  //остановка почтового сервере Wiser
+  public void stop() {
+    wiser.stop();
+  }
+}
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,11 +1,45 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase{
 
+  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
   @Test
-  public void testRegistration() {
-    app.registration().start("user1", "user1@localhost.localdomain");
+  public void testRegistration() throws IOException, MessagingException {
+    long now = System.currentTimeMillis();                                            //создаем уникальные идентификаторы для пользователей, чтобы каждый раз регистрировался новый пользователь (используемая функция возвращает текущее время в миллисекундах)
+    String user = String.format("user%s", now);
+    String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);                //(1) параметр шаблон; (2) то, что должно подставиться вместо %s
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);   //ожидаение письма: ждём 2 письма, ждём 10000 миллисекунд
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);                           //завершаем регистрацию
+    assertTrue (app.newSession().login(user, password));                             //авторизация: проверяем, что регистрация прошла успешно
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();    //ищем среди всех писем, все письма, отправленные на нужный адрес и берём первое
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();       //регулярное выражение: ищем текст, который начинается с http://, после которого идут 1 и более непробельных символов
+    return regex.getText(mailMessage.text);                                                                 //применяем регулярное выражение к тексту письма и возвращаем получивашееся значение
+  }
+
+  @AfterMethod (alwaysRun = true)   //почтовый сервер будет останавливаться, даже если тест завершится не успешно
+  public void stopMailServer() {
+    app.mail().stop();
   }
 }
