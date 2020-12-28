dee48ee7d6416667a2ff9d8d50febd5c292c249e
Seeker14 <allegoriy@list.ru>
2020-12-29 01:41:39 +0300

    [Task 19] Added integration of tests with bug tracker

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
@@ -9,29 +11,51 @@ import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.util.Set;
 
-import static org.testng.Assert.assertEquals;
 
 
 public class SoapTests extends TestBase {
+
+  int issueId = 2;
+
   @Test
   public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
-    Set<Project> projects = app.soap().getProjects();
-    System.out.println(projects.size());
-    for(Project project: projects){
-      System.out.println(project.getName());
+    try {
+      skipIfNotFixed(issueId);
+      Set<Project> projects = app.soap().getProjects();
+      System.out.println(projects.size());
+      for (Project project : projects) {
+        System.out.println(project.getName());
+      }
+    } catch (SkipException e) {
+      e.printStackTrace();
     }
   }
 
   @Test
   public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
-    Set<Project> projects = app.soap().getProjects();
-    Issue issue = new Issue().withSummary("Test issue")
-            .withDescription("Test issue description").withProject(projects.iterator().next());
-
-   Issue created =  app.soap().addIssue(issue);
-   assertEquals(issue.getSummary(),created.getSummary());
+    try {
+      skipIfNotFixed(issueId);
+      long now = System.currentTimeMillis();
+      Set<Project> projects = app.soap().getProjects();
+      Issue issue = new Issue().withSummary(String.format("Summary issue %s", now))
+              .withDescription(String.format("Description issue %s ", now))
+              .withProject(projects.iterator().next());
+      Issue created = app.soap().addIssue(issue);
+      Assert.assertEquals(issue.getSummary(), created.getSummary());
+    } catch (SkipException e) {
+      e.printStackTrace();
+    }
   }
 
-
+  @Test
+  public void testOnBugStatus () throws RemoteException,  ServiceException, MalformedURLException {
+    try {
+      skipIfNotFixed(issueId);
+      if (isIssueOpen(issueId)) {
+        System.out.println("BUG OPEN");
+      } else System.out.println("BUG Closed");
+    } catch (SkipException e) {
+      e.printStackTrace();
+    }
+  }
 }
-
